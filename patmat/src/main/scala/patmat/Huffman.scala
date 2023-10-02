00005b1aed3676ea72ebd94bcb45e28411298a76
mgmg@macbook <101696461+qwercxzsda@users.noreply.github.com>
2023-10-02 19:50:59 +0900

    feat: implement function makeOrderedLeafList

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -89,7 +89,10 @@ object Huffman {
    * head of the list should have the smallest weight), where the weight
    * of a leaf is the frequency of the character.
    */
-  def makeOrderedLeafList(freqs: List[(Char, Int)]): List[Leaf] = ???
+  def makeOrderedLeafList(freqs: List[(Char, Int)]): List[Leaf] = {
+    val sortedFreps = freqs sortWith(_._2 < _._2)
+    sortedFreps map (x => Leaf(x._1, x._2))
+  }
 
   /**
    * Checks whether the list `trees` contains only one single code tree.
