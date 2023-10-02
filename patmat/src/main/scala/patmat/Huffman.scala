41ec0d7103979e63bdf04ba598423fa24ee42108
mgmg@macbook <101696461+qwercxzsda@users.noreply.github.com>
2023-10-03 03:06:47 +0900

    feat: implement Part 4b, Encoding using code table

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -239,7 +239,12 @@ object Huffman {
    * This function returns the bit sequence that represents the character `char` in
    * the code table `table`.
    */
-  def codeBits(table: CodeTable)(char: Char): List[Bit] = ???
+  def codeBits(table: CodeTable)(char: Char): List[Bit] = {
+    table find (_._1 == char) match {
+      case Some((_, bits)) => bits
+      case None => throw new IllegalArgumentException("Character not found in code table")
+    }
+  }
 
   /**
    * Given a code tree, create a code table which contains, for every character in the
@@ -249,14 +254,24 @@ object Huffman {
    * a valid code tree that can be represented as a code table. Using the code tables of the
    * sub-trees, think of how to build the code table for the entire tree.
    */
-  def convert(tree: CodeTree): CodeTable = ???
+  def convert(tree: CodeTree): CodeTable = {
+    tree match {
+      case Leaf(char, _) => List((char, List()))
+      case Fork(left, right, _, _) =>
+        val leftTable = convert(left)
+        val rightTable = convert(right)
+        val leftTableNew = leftTable map (x => (x._1, 0 :: x._2))
+        val rightTableNew = rightTable map (x => (x._1, 1 :: x._2))
+        mergeCodeTables(leftTableNew, rightTableNew)
+    }
+  }
 
   /**
    * This function takes two code tables and merges them into one. Depending on how you
    * use it in the `convert` method above, this merge method might also do some transformations
    * on the two parameter code tables.
    */
-  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = ???
+  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = a ::: b
 
   /**
    * This function encodes `text` according to the code tree `tree`.
@@ -264,5 +279,8 @@ object Huffman {
    * To speed up the encoding process, it first converts the code tree to a code table
    * and then uses it to perform the actual encoding.
    */
-  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = ???
+  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = {
+    val codeTable = convert(tree)
+    (text foldRight List[Bit]())((char: Char, acc: List[Bit]) => codeBits(codeTable)(char) ::: acc)
+  }
 }
