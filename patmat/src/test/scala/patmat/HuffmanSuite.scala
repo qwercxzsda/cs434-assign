e3f7dec7af55709f41a962dbedb332a9bf61485c
mgmg@macbook <101696461+qwercxzsda@users.noreply.github.com>
2023-10-03 03:07:30 +0900

    feat: add test cases

--- patmat/src/test/scala/patmat/HuffmanSuite.scala
+++ patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -30,6 +30,10 @@ class HuffmanSuite extends FunSuite {
     assert(string2Chars("hello, world") === List('h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd'))
   }
 
+  test("times") {
+    assert(times(List('a', 'b', 'a')) === List(('a', 2), ('b', 1)) || times(List('a', 'b', 'a')) === List(('b', 1), ('a', 2)))
+  }
+
   test("makeOrderedLeafList for some frequency table") {
     assert(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))) === List(Leaf('e',1), Leaf('t',2), Leaf('x',3)))
   }
@@ -39,9 +43,38 @@ class HuffmanSuite extends FunSuite {
     assert(combine(leaflist) === List(Fork(Leaf('e',1),Leaf('t',2),List('e', 't'),3), Leaf('x',4)))
   }
 
+  test("until") {
+    val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
+    val combined = combine(combine(leaflist))
+    assert(until(singleton, combine)(leaflist) === combined)
+  }
+
+  test("createCodeTree") {
+    val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
+    val combined = combine(combine(leaflist))
+    assert(createCodeTree(string2Chars("textxxx")) === combined.head)
+  }
+
+  test("secret") {
+    assert(decodedSecret === string2Chars("huffmanestcool"))
+  }
+
   test("decode and encode a very short text should be identity") {
     new TestTrees {
       assert(decode(t1, encode(t1)("ab".toList)) === "ab".toList)
+    }
+  }
+
+  test("quickEncode") {
+    new TestTrees {
+      assert(decode(t1, quickEncode(t1)("ab".toList)) === "ab".toList)
+    }
+  }
+
+  test("quickEncode2") {
+    new TestTrees {
+      val str = "sdxjfsddxjjjssffddxxxjjjjffds"
+      assert(decode(frenchCode, quickEncode(frenchCode)(str.toList)) === str.toList)
     }
   }
 }
