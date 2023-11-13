0b6f911d1e122b7ae794d5e0e45dfebf7b092f00
mgmg@macbook <101696461+qwercxzsda@users.noreply.github.com>
2023-11-13 21:45:43 +0900

    feat: implement continueWith

--- nodescala/src/main/scala/nodescala/package.scala
+++ nodescala/src/main/scala/nodescala/package.scala
@@ -108,7 +108,11 @@ package object nodescala {
      *  The function `cont` is called only after the current future completes.
      *  The resulting future contains a value returned by `cont`.
      */
-    def continueWith[S](cont: Future[T] => S): Future[S] = ???
+    def continueWith[S](cont: Future[T] => S): Future[S] = {
+      for {
+        _ <- f
+      } yield cont(f)
+    }
 
     /** Continues the computation of this future by taking the result
      *  of the current future and mapping it into another future.
