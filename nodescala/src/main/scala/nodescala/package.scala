a7c7d1726f21259f60e08cfdcafbd89dd4fcd25f
mgmg@macbook <101696461+qwercxzsda@users.noreply.github.com>
2023-11-13 21:55:07 +0900

    feat: implement continue

--- nodescala/src/main/scala/nodescala/package.scala
+++ nodescala/src/main/scala/nodescala/package.scala
@@ -120,7 +120,11 @@ package object nodescala {
      *  The function `cont` is called only after the current future completes.
      *  The resulting future contains a value returned by `cont`.
      */
-    def continue[S](cont: Try[T] => S): Future[S] = ???
+    def continue[S](cont: Try[T] => S): Future[S] = {
+      val p = Promise[S]()
+      f onComplete (x => p success cont(x))
+      p.future
+    }
 
   }
 
