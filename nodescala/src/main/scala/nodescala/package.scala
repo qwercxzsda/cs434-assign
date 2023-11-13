44d8c6fffbfd412e76bd00b9d2f9c4304a01728d
mgmg@macbook <101696461+qwercxzsda@users.noreply.github.com>
2023-11-13 21:00:33 +0900

    feat: implement all

--- nodescala/src/main/scala/nodescala/package.scala
+++ nodescala/src/main/scala/nodescala/package.scala
@@ -35,7 +35,14 @@ package object nodescala {
      *  The values in the list are in the same order as corresponding futures `fs`.
      *  If any of the futures `fs` fails, the resulting future also fails.
      */
-    def all[T](fs: List[Future[T]]): Future[List[T]] = ???
+    def all[T](fs: List[Future[T]]): Future[List[T]] = {
+      fs match {
+        case Nil => Future(Nil)
+        case e :: tail => async {
+          await(e) :: await(all(tail))
+        }
+      }
+    }
     /** Given a list of futures `fs`, returns the future holding the value of the future from `fs` that completed first.
      *  If the first completing future in `fs` fails, then the result is failed as well.
      *
