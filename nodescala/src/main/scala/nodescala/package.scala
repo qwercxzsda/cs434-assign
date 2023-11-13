0cc8f35c88f61fe843c5c29c53a84d7aa593bdd5
mgmg@macbook <101696461+qwercxzsda@users.noreply.github.com>
2023-11-13 21:08:05 +0900

    feat: implement any

--- nodescala/src/main/scala/nodescala/package.scala
+++ nodescala/src/main/scala/nodescala/package.scala
@@ -52,7 +52,11 @@ package object nodescala {
      *
      *  may return a `Future` succeeded with `1`, `2` or failed with an `Exception`.
      */
-    def any[T](fs: List[Future[T]]): Future[T] = ???
+    def any[T](fs: List[Future[T]]): Future[T] = {
+      val p = Promise[T]()
+      fs foreach { (f: Future[T]) => f onComplete { x => p tryComplete x } }
+      p.future
+    }
 
     /** Returns a future with a unit value that is completed after time `t`.
      */
